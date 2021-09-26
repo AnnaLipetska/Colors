bd4ab63952fee5fe1ab564a7e0f69305350b3470
Anna <a.lipetska@gmail.com>
2021-09-26 11:35:51 +0300

    $ - refactoring added

---
+++ Refactoring/Refactoring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Refactoring
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Func<int, int> triple = (int number) => number * 3;
+            var results = new int[] { 100, 250 }.Select(i => triple(i)).ToArray();
+            string info = string.Empty;
+            for (int i = 0; i < results.Length; i++)
+            {
+                info += (results[i] > 500) ? $"Result {i + 1}: {results[i]} " : string.Empty;
+            }
+
+            info = info.Trim();
+            if (!string.IsNullOrEmpty(info))
+                Console.WriteLine(info);
+        }
+    }
+}
