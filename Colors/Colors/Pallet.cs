5d3670f8e941fec61561c4263aa1f38cbf377511
Anna <a.lipetska@gmail.com>
2021-09-26 11:10:30 +0300

    $ - some refactoring done

---
+++ Colors/Colors/Pallet.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Colors
+{
+    class Pallet
+    {
+        readonly Bitmap _bmp;
+
+        public Pallet(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    _bmp = new Bitmap(fileName);
+                }
+                catch
+                {
+                    throw new Exception("Not a bitmap image!!!");
+                }
+            }
+        }
+
+        public IEnumerable<KeyValuePair<Color, int>> GetMostFrequentColors(int number)
+        {
+            var colors = GetAllColors();
+            return colors.OrderByDescending(c => c.Value).Take(number);
+        }
+
+        IDictionary<Color, int> GetAllColors()
+        {
+            var colors = new Dictionary<Color, int>();
+
+            if (_bmp != null)
+            {
+                for (int y = 0; y < _bmp.Size.Height; y++)
+                {
+                    for (int x = 0; x < _bmp.Size.Width; x++)
+                    {
+                        var pixel = _bmp.GetPixel(x, y);
+                        if (colors.ContainsKey(pixel))
+                        {
+                            colors[pixel] = colors[pixel] + 1;
+                        }
+                        else
+                        {
+                            colors[pixel] = 1;
+                        }
+                    }
+                }
+            }
+
+            return colors;
+        }
+
+        public int CountImageColors()
+        {
+            var count = 0;
+            var colors = new HashSet<Color>();
+
+            if (_bmp != null)
+            {
+                for (int y = 0; y < _bmp.Size.Height; y++)
+                {
+                    for (int x = 0; x < _bmp.Size.Width; x++)
+                    {
+                        colors.Add(_bmp.GetPixel(x, y));
+                    }
+                }
+                count = colors.Count;
+            }
+
+            return count;
+        }
+    }
+}
--- Colors/Colors/Program.cs
+++ Colors/Colors/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 
 namespace Colors
@@ -12,13 +11,14 @@ namespace Colors
         {
             var fileName = @"D:\my_photo.jpg";
 
-            var number = CountImageColors(fileName);
+            var pallet = new Pallet(fileName);
 
-            Console.WriteLine(number);
+            var number = pallet.CountImageColors();
 
-            IDictionary<Color, int> colors = GetAllColors(fileName);
+            Console.WriteLine($"Total number of colors: {number}");
 
-            var frequentColors = GetMostFrequentColors(colors, 10).ToList();
+            var frequentColors = pallet.GetMostFrequentColors(10).ToList();
+
             foreach (KeyValuePair<Color, int> color in frequentColors)
             {
                 Console.WriteLine(color.Key + " -> " + color.Value);
@@ -26,88 +26,6 @@ namespace Colors
 
 
             Console.ReadKey();
-        }
-
-        private static IEnumerable<KeyValuePair<Color, int>> GetMostFrequentColors(IDictionary<Color, int> colors, int number)
-        {
-            return colors.OrderByDescending(c => c.Value).Take(number);
-        }
-
-        private static IDictionary<Color, int> GetAllColors(string fileName)
-        {
-            var colors = new Dictionary<Color, int>();
-            Bitmap bmp = null;
-
-            if (File.Exists(fileName))
-            {
-                try
-                {
-                    bmp = new Bitmap(fileName);
-                    if (bmp != null)
-                    {
-                        for (int y = 0; y < bmp.Size.Height; y++)
-                        {
-                            for (int x = 0; x < bmp.Size.Width; x++)
-                            {
-                                var pixel = bmp.GetPixel(x, y);
-                                if (colors.ContainsKey(pixel))
-                                {
-                                    colors[pixel] = colors[pixel] + 1;
-                                }
-                                else
-                                {
-                                    colors[pixel] = 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                catch
-                {
-                    throw;
-                }
-                finally
-                {
-                    bmp.Dispose();
-                }
-            }
-            return colors;
-        }
-
-        public static int CountImageColors(string fileName)
-        {
-            var count = 0;
-            var colors = new HashSet<Color>();
-            Bitmap bmp = null;
-
-            if (File.Exists(fileName))
-            {
-                try
-                {
-                    bmp = new Bitmap(fileName);
-                    if (bmp != null)
-                    {
-                        for (int y = 0; y < bmp.Size.Height; y++)
-                        {
-                            for (int x = 0; x < bmp.Size.Width; x++)
-                            {
-                                colors.Add(bmp.GetPixel(x, y));
-                            }
-                        }
-                        count = colors.Count;
-                    }
-                }
-                catch
-                {
-                    throw;
-                }
-                finally
-                {
-                    colors.Clear();
-                    bmp.Dispose();
-                }
-            }
-            return count;
         }
     }
 }
