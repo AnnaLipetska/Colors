06ba58fd1bfd6b0cc6ce039a2b154e0a74e6c2a6
Anna <a.lipetska@gmail.com>
2021-10-10 14:23:10 +0300

    $ - trying to form proper request

--- Colors/Colors/Program.cs
+++ Colors/Colors/Program.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using PalletLib;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Text;
+using RestSharp;
 
 namespace Colors
 {
@@ -10,6 +16,9 @@ namespace Colors
     {
         static void Main(string[] args)
         {
+
+            DoWork();
+
             //var fileName = @"D:\test1.jpg";
             var fileName = @"D:\my_photo.jpg";
             //var fileName = @"D:\small.jpg";
@@ -29,6 +38,30 @@ namespace Colors
 
 
             Console.ReadKey();
+        }
+
+        private static void DoWork()
+        {
+            var client = new RestClient("https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs?Authorization=183d315df7b4488a9c177c70f29f9dcd");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Key a91dbe59965a47f1a5ad0a269f460abb");
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{  " + "\n" +
+            @"   ""inputs"":[  " + "\n" +
+            @"      {  " + "\n" +
+            @"         ""data"":{  " + "\n" +
+            @"            ""image"":{  " + "\n" +
+            @"               ""url"":""https://www.thoughtco.com/thmb/07W0mpQ_K2BG2GNdi1AiFFlUDeI=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/chemistry-glassware-56a12a083df78cf772680235.jpg""" + "\n" +
+            @"            }" + "\n" +
+            @"         }" + "\n" +
+            @"      }" + "\n" +
+            @"   ]" + "\n" +
+            @"}" + "\n" +
+            @"";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
         }
     }
 }
