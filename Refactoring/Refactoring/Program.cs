dfd6f10bb5a092f1b40b1427bd75e99ab3058d98
Anna <a.lipetska@gmail.com>
2021-10-10 11:38:26 +0300

    $ - refactoring changes

--- Refactoring/Refactoring/Program.cs
+++ Refactoring/Refactoring/Program.cs
@@ -8,11 +8,10 @@ namespace Refactoring
         public static void Main()
         {
             const int MIN = 500;
-            Func<int, int> triple = (int number) => number * 3;
-            var results = new int[] { 100, 250 }.Select(i => triple(i)).ToArray();
+            var results = new int[] { 100, 250 }.Select(i => i * 3).ToList();
 
             string info = string.Empty;
-            for (int i = 0; i < results.Length; i++)
+            for (int i = 0; i < results.Count; i++)
             {
                 info += (results[i] > MIN) ? $"Result {i + 1}: {results[i]} " : string.Empty;
             }
